bac80e36d723b63c15f80f7b5f37ffcafb56fe91
Jyotsana Joshi <joshijyotsana1212@gmail.com>
2023-03-22 18:31:19 +0530

    Made year and month dynamic

--- src/app/components/drilldown-chart/drilldown-chart.component.ts
+++ src/app/components/drilldown-chart/drilldown-chart.component.ts
@@ -15,40 +15,68 @@ export class DrilldownChartComponent implements OnInit {
 
   private chart!: Highcharts.Chart;
   options: any
-  amount2020: number = 0;
-  amount2021: number = 0;
-  amount2022: number = 0;
-  amount2023: number = 0;
-  finalData: any = []
-  dataOf20 = ['Jan 20', 'Feb 20', 'Mar 20', 'Apr 20', 'May 20', 'Jun 20', 'Jul 20', 'Aug 20', 'Sep 20', 'Oct 20', 'Nov 20', 'Dec 20',]
-  dataOf21 = ['Jan 21', 'Feb 21', 'Mar 21', 'Apr 21', 'May 21', 'Jun 21', 'Jul 21', 'Aug 21', 'Sep 21', 'Oct 21', 'Nov 21', 'Dec 21',]
-  dataOf22 = ['Jan 22', 'Feb 22', 'Mar 22', 'Apr 22', 'May 22', 'Jun 22', 'Jul 22', 'Aug 22', 'Sep 22', 'Oct 22', 'Nov 22', 'Dec 22',]
-  dataOf23 = ['Jan 23', 'Feb 23', 'Mar 23', 'Apr 23', 'May 23', 'Jun 23', 'Jul 23', 'Aug 23', 'Sep 23', 'Oct 23', 'Nov 23', 'Dec 23',]
-
+  finalData: any = [];
+  drillDownResult: any = [];
+  yearData: any = [];
   constructor() { }
 
-  async prepareChartData() {
-
-    let data = jsonData.data
+private prepareChartData() {
+    let data = jsonData.data;
+    let result: any = [];
+    let months: any = [];
+    let year: any = [];
+    let sum: any = {};
     data.map((item: any) => {
-      item.periods.map((x: any) => {
-        if (x.year == 2020) {
-          this.amount2020 += x.amt;
-        } else if (x.year == 2021) {
-          this.amount2021 += x.amt;
-        } else if (x.year == 2022) {
-          this.amount2022 += x.amt;
-        } else if (x.year == 2023) {
-          this.amount2023 += x.amt;
+      const reduceData = item.periods.reduce((acc: any, { year, amt, period }: any) => {
+        if (!acc[year]) {
+          acc[year] = amt;
+          acc.period.push({ period, year })
+        } else {
+          acc[year] += amt;
+          acc.period.push({ period, year })
         }
-      });
+        return acc;
+      }, { period: [] });
+      result.push(reduceData);
     });
+   
+    for (let i = 0; i < result.length; i++) {
+      let obj = result[i];
+      for (let key in obj) {
+        if (key in sum) {
+          sum[key] += obj[key];
+        } else {
+          sum[key] = obj[key];
+        }
+      }
+    }
 
+    Object.keys(sum).map(res => {
+      this.yearData.push({
+        name: `${res}`,
+        y: sum[res],
+        drilldown: `${res}`
+      });
+
+    });
+    this.yearData.splice(-1);
+
+    result.map((item: any) => {
+      item.period.map((r: any) => {
+        months.push(r.period);
+        year.push(r.year)
+      })
+    });
+    const d = [... new Set(months)];
+    const y = [... new Set(year)];
+
+    this.filterData(d, y)
   }
 
-  filterData(period: any) {
+  // filter data using period and year for drilldown
+  filterData(period: any, year: any) {
     this.finalData = [];
-    let data = jsonData.data
+    let data = jsonData.data;
     period.map((month: any) => {
       let total = 0;
       data.map((item: any) => {
@@ -56,11 +84,36 @@ export class DrilldownChartComponent implements OnInit {
           if (itm.period == month) {
             total += itm.amt;
           }
-        })
+        });
       });
-      this.finalData.push([month, total]);
+
+      this.finalData.push({ month, total });
     });
-    return this.finalData
+    const dataWithYear = this.finalData.map((d: any) => {
+      const lastTwoDigits = d.month.slice(-2); // get last two digits of month
+      const matchingYear = year.find((y: any) => lastTwoDigits == y % 100); // find matching year
+      return {
+        ...d,
+        year: matchingYear ? matchingYear : null // add year property to object
+      };
+    });
+
+    // create a new object for each year
+    dataWithYear.forEach((item: any) => {
+      const year = item.year.toString();;
+      const existingObj = this.drillDownResult.find((obj: any) => obj.id === year);
+
+      if (existingObj) {
+        existingObj.data.push([item.month, item.total]);
+      } else {
+        this.drillDownResult.push({
+          name: `${year}`,
+          id: `${year}`,
+          data: [[item.month, item.total]]
+        });
+      }
+    });
+
   }
 
 
@@ -72,7 +125,7 @@ export class DrilldownChartComponent implements OnInit {
     this.options = {
       chart: {
         type: 'spline',
-    },
+      },
       title: {
         text: 'Revenue by Year and Month',
       },
@@ -84,7 +137,7 @@ export class DrilldownChartComponent implements OnInit {
           text: 'Revenue',
         },
       },
-    
+
       plotOptions: {
         line: {
           marker: {
@@ -101,59 +154,17 @@ export class DrilldownChartComponent implements OnInit {
       series: [
         {
           name: 'Year',
-          data: [
-            {
-              name: '2020',
-              y: this.amount2020,
-              drilldown: '2020',
-            },
-            {
-              name: '2021',
-              y: this.amount2021,
-              drilldown: '2021',
-            },
-            {
-              name: '2022',
-              y: this.amount2022,
-              drilldown: '2022',
-            },
-            {
-              name: '2023',
-              y: this.amount2023,
-              drilldown: '2023',
-            },
-          ],
+          data: this.yearData
         },
       ],
       drilldown: {
-        series: [
-          {
-            name: '2020',
-            id: '2020',
-            data: this.filterData(this.dataOf20)
-          },
-          {
-            name: '2021',
-            id: '2021',
-            data: this.filterData(this.dataOf21)
-          },
-          {
-            name: '2022',
-            id: '2022',
-            data: this.filterData(this.dataOf22)
-          },
-          {
-            name: '2023',
-            id: '2023',
-            data: this.filterData(this.dataOf23)
-          },
-        ],
+        series: this.drillDownResult
       },
       tooltip: {
         backgroundColor: '#FFFFFF',
         borderColor: '#000000',
         borderRadius: 5,
-        padding: 10     
+        padding: 10
       },
     }
     this.chart = Highcharts.chart(this.chartElement.nativeElement, this.options);
